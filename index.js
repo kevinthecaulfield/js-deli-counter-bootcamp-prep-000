b078e96954302839545876a3352ea85303bc6c9e
Kevin Smith <kevincaulfield630@gmail.com>
2017-11-12 23:27:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function takeANumber(katzDeliLine,name) {
   var katzDeliLine = []
-  katzDeliLine.push(`${name}`)
+  katzDeliLine.push(`Welcome, ${name}. You are ${katzDeliLine.length + 1} in line` )
   return katzDeliLine
 }
